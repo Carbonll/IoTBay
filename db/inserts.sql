a2333b6765f179ff4eda486cd2969df05c17219b
MelvinHK <53848567+MelvinHK@users.noreply.github.com>
2021-05-09 02:50:13 +1000

    uncommented sql files

--- db/inserts.sql
+++ db/inserts.sql
@@ -9,19 +9,13 @@
  * Created: 08/05/2021
  */
 
--- INSERT INTO IOTUSER."USER" 
---     ("NAME", EMAIL, PASSWORD, ROLE_ID) VALUES
---     ('admin', 'admin@iotbay.com', 'admin', 1);
+INSERT INTO IOTUSER.ROLE
+    (ID, ROLE_NAME) VALUES
+    (1, 'System Admin'),
+    (2, 'Staff'),
+    (3, 'Customer');
 
--- INSERT INTO IOTUSER.ROLE
---     (ID, ROLE_NAME) VALUES
---     (1, 'System Admin'),
---     (2, 'Staff'),
---     (3, 'Customer');
+INSERT INTO IOTUSER."USER" 
+    ("NAME", EMAIL, PASSWORD, ROLE_ID) VALUES
+    ('admin', 'admin@iotbay.com', 'admin', 1);
 
--- DELETE FROM IOTUSER.AUDITS;
--- DELETE FROM IOTUSER."USER";
-
-INSERT INTO IOTUSER.AUDITS
-    (USER_ID, AUDIT_EVENT, AUDIT_DATE) VALUES
-    (100022, 'Test', '01-01-2021 test');
