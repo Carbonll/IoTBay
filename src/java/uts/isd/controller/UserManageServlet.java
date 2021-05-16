012ca6147ed4509dc1fe415746084a8525e20bc7
Jessehazeldine <jesse@jesses-air>
2021-05-16 13:32:48 +1000

    arrangements of search (if statements) have been altered

--- src/java/uts/isd/controller/UserManageServlet.java
+++ src/java/uts/isd/controller/UserManageServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import uts.isd.model.*;
+import uts.isd.model.User;
 import uts.isd.model.dao.DBManager;
 
 public class UserManageServlet { //Needs work // For search Function
@@ -30,8 +30,8 @@ public class UserManageServlet { //Needs work // For search Function
         Validator validator = new Validator();//?
         session.setAttribute("nameErr", "");
         session.setAttribute("phoneErr", "");
-        User user = (User) session.getAttribute("user"); //Not sure if used properly, probz should be a aviable linked to searched values
-        ArrayList<User> fetchUsers = new ArrayList();
+        //User user = (User) session.getAttribute("user"); //Not sure if used properly, probz should be a aviable linked to searched values
+        ArrayList<User> users = new ArrayList();
         DBManager manager = (DBManager) session.getAttribute("manager");
 
         String name = request.getParameter("name"); //where is it recieving the parameter from, what is the value?
@@ -40,35 +40,38 @@ public class UserManageServlet { //Needs work // For search Function
         String phoneErr = request.getParameter("phoneErr");
 
         try {
-            if (name == null && phone == null) {
-                fetchUsers = manager.fetchUsers();
-                session.setAttribute("fetchUsers", fetchUsers);
+            if (name == null && phone == null) { //no search = all users
+                users = manager.fetchUsers();
+                session.setAttribute("users", users);
                 request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
 
-            } else if (phone == null && name != null) {
-                user = manager.findUserByName(user.getName());
+            } else if (phone == null && name != null) { // search by name = all userID data where name = name searched
+                session.setAttribute("users", users);
                 request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
-            } else if (name == null && phone != null) {
-                user = manager.findUserByPhone(user.getPhone());
-                request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
-
-            } else {
+                
                 if (validator.validateName(name)) {
-                    user = manager.findUserByName(user.getName());
+                    users = manager.findUserByName(name); // Not sure how to apply the sql requry method using the inputs of the search function on the jsp page, to put into a list which can then be put back to the jsp table for viewing the selected data 
+                    session.setAttribute("users", users);
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
                 } else {
                     session.setAttribute("nameErr", "Invalid name format");
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
                 }
+                
+            } else { //(name == null && phone != null) {  // search by phone = all userID data where phone = phone searched
+                session.setAttribute("users", users);
+                request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
+                
                 if (validator.validatePhone(phone)) {
-                    user = manager.findUserByPhone(user.getPhone());
+                    users = manager.findUserByPhone(phone); // Not sure how to apply the sql requry method using the inputs of the search function on the jsp page, to put into a list which can then be put back to the jsp table for viewing the selected data 
+                    session.setAttribute("users", users);
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
                 } else {
                     session.setAttribute("phoneErr", "Invalid phone number format");
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
                 }
-
-            }
+        }
+                
         } catch (SQLException ex) {
             System.out.println(ex.getMessage() == null ? "Unable to view an account" : " Account acquired");
         }
