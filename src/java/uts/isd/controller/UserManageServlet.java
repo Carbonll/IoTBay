e864d24bfea809421ad1f49fe0d91a48d72f0b58
Jessehazeldine <jesse@jesses-air>
2021-05-16 10:45:34 +1000

    Used in the search function for usermanage.jsp, needs work

--- src/java/uts/isd/controller/UserManageServlet.java
+++ src/java/uts/isd/controller/UserManageServlet.java
@@ -22,56 +22,55 @@ import javax.servlet.http.HttpSession;
 import uts.isd.model.*;
 import uts.isd.model.dao.DBManager;
 
-public class UserManageServlet {
+public class UserManageServlet { //Needs work // For search Function
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        Validator validator = new Validator();
-
+        Validator validator = new Validator();//?
         session.setAttribute("nameErr", "");
         session.setAttribute("phoneErr", "");
-        User user = (User) session.getAttribute("user");
-        ArrayList<Audit> log = new ArrayList();
+        User user = (User) session.getAttribute("user"); //Not sure if used properly, probz should be a aviable linked to searched values
+        ArrayList<User> fetchUsers = new ArrayList();
         DBManager manager = (DBManager) session.getAttribute("manager");
 
-        String name = request.getParameter("name");
+        String name = request.getParameter("name"); //where is it recieving the parameter from, what is the value?
         String nameErr = request.getParameter("nameErr");
         String phone = request.getParameter("phone");
         String phoneErr = request.getParameter("phoneErr");
 
         try {
-            if (name == null) {
-                log = manager.fetchAuditsByUserID(user.getID());
-                session.setAttribute("log", log);
+            if (name == null && phone == null) {
+                fetchUsers = manager.fetchUsers();
+                session.setAttribute("fetchUsers", fetchUsers);
                 request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
-            } else if (phone == null) {
-                log = manager.fetchAuditsByname(user.getID());
-                session.setAttribute("log", log);
-                request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);   
+
+            } else if (phone == null && name != null) {
+                user = manager.findUserByName(user.getName());
+                request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
+            } else if (name == null && phone != null) {
+                user = manager.findUserByPhone(user.getPhone());
+                request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
+
             } else {
-                if (validator.validateDate(name)) {
-                    log = manager.fetchAuditsByDate(user.getID(), name);
-                    session.setAttribute("log", log);
+                if (validator.validateName(name)) {
+                    user = manager.findUserByName(user.getName());
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
                 } else {
                     session.setAttribute("nameErr", "Invalid name format");
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
-                }      
-                if (validator.validateDate(phone)) {
-                    log = manager.fetchAuditsByDate(user.getID(), phone);
-                    session.setAttribute("log", log);
+                }
+                if (validator.validatePhone(phone)) {
+                    user = manager.findUserByPhone(user.getPhone());
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
                 } else {
                     session.setAttribute("phoneErr", "Invalid phone number format");
                     request.getRequestDispatcher("SysAdminUserManage.jsp").include(request, response);
                 }
-                
+
             }
         } catch (SQLException ex) {
-            System.out.println(ex.getMessage() == null ? "Unable to obtain logs" : "Logs acquired");
+            System.out.println(ex.getMessage() == null ? "Unable to view an account" : " Account acquired");
         }
     }
 }
-
-
